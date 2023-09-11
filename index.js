83c1bc25e56645bea9a3485b2aa2ab75af355286
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-09-11 19:50:14 -0400

    control flow  lab complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feets){
+  let result
+  if (feets<=400){
+  result = 'This one is on me!'
+  }
+else if (feets<=2000){
+result = 'That will be twenty bucks.'
+}
+else if (feets<=2500){
+result = 'I will gladly take your thirty bucks.'
+}
+else if (feets>2500){
+result = 'No can do.'
+}
+return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+switch(tips){
+case 'generous' :
+  return ("Thank you so much.");
+  case 'not as generous' :
+    return ('Thank you.')
+    default:
+      return ("Bye.")
+}
+  
+
 }
